2642bea2e1ec8621e0e24bac3b907cf106f3211e
Milton <sandovalmonasteriom@gmail.com>
2024-08-13 15:08:50 -0400

    Se modifico el programa Practica1 para practicar el redondeo a dos decimales

--- Practicas c#/Program.cs
+++ Practicas c#/Program.cs
@@ -1,46 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Practica
 {
     class Practica1
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  
         {
-            
-            int[,] DosBloques = new int[2,10];
-            Console.WriteLine();
-            for (int i = 0; i < 2; i++)
+            Console.WriteLine(Num(5));
+        }
+        public static string Num(int n)
+        {
+            int Contador = 1;
+            double Dividendo = 4;
+            double A = 1;
+            while (n > Contador)
             {
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.Write("Ingresa el valor " + (i+1) + " en la posicion:" + j +":");
-                    DosBloques[i,j] = int.Parse(Console.ReadLine());
-                }
+                A += (1 / Dividendo);
+                Dividendo += 3;
+                Contador++;
             }
-
-            int contador = 0, mayor1 = DosBloques[0,0], mayor2 = DosBloques[1, 0];
-            foreach (var item in DosBloques)
-            {
-                if(contador == 10)
-                {
-                    Console.WriteLine("--------------------------------------------------------");
-                }else if(contador < 10)
-                {
-                    if(mayor1<item)
-                        mayor1 = item;
-                }else if (contador >= 10)
-                {
-                    if (mayor2 < item)
-                        mayor2 = item;
-                }
-                contador++;
-            }
-            Console.WriteLine($"El numero mayor del primer bloque es:{mayor1}\n" +
-                $"El numero mayor del segundo bloque es:{mayor2}");
-
-
-
+            return A.ToString("F2");
         }
     }
+        
 }
