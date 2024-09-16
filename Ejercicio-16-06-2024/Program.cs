1f434116062836e1e64f951274915f7613d52129
Milton <sandovalmonasteriom@gmail.com>
2024-09-16 10:35:55 -0400

    Se practico el manejo de string y metodos del mismo

---
+++ Ejercicio-16-06-2024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Practicas
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpinWords("Hey fellow warriors"));
+        }
+
+        public static string SpinWords(string sentencia)
+        {
+            var a = sentencia.Split(' ');
+            string b = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Count() >= 5)
+                    b += new string(a[i].Reverse().ToArray());
+                else
+                    b += a[i];
+                if (i < a.Length - 1)
+                    b += " ";
+            }
+            return b;
+        }
+
+    }
+}   
