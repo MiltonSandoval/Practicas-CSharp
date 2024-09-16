d3d0241f11522b4cdd24b2914dd3fa95e007beb2
Milton <sandovalmonasteriom@gmail.com>
2024-09-16 10:58:48 -0400

    Ejercicio para practicar la logica con string , aun me falta mejorar la logica, el codigo quedo muy extenso.

---
+++ ParImparString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Practicas
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Par("Weird string case"));
+        }
+
+        public static string Par(string sentencia)
+        {
+            string Carri = "";
+            bool Controlador = true;
+            foreach (var item in sentencia)
+            {
+                if(Controlador)
+                {
+                    if (item.ToString() == " ")
+                    {
+                        Carri += item.ToString();
+                        Controlador = true;
+                    }
+                    else
+                    {
+                        Carri += item.ToString().ToUpper();
+                        Controlador = false;
+                    }
+                   
+                }    
+                else
+                {
+                    Carri += item.ToString().ToLower();
+                    Controlador = true;
+                }
+
+            }
+            return Carri;
+        }
+
+    }
+}
