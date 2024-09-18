af701f3a93e7b89853689f18ca18b1c1b20e1746
Milton <sandovalmonasteriom@gmail.com>
2024-09-18 09:39:01 -0400

    retornos de cadenas de string al comprar si existe sub cadena de a en b

---
+++ Retornos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] a1 = { "arp", "live", "strong" };
+        string[] a2 = { "lively", "alive", "harp", "sharp", "armstrong" };
+
+       
+        var result = a1
+            .Where(s1 => a2.Any(s2 => s2.Contains(s1))) 
+            .OrderBy(s => s)
+            .ToArray();
+        Console.WriteLine(string.Join(", ", result)); 
+    }
+}
+
+
+
+    
