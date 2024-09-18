d2b222b6c09e78978e77fa54fd73ab7d1d35561d
Milton <sandovalmonasteriom@gmail.com>
2024-09-18 10:53:36 -0400

    practica de manejo de cademas y linq

---
+++ Concatenacion De strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Practica
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = LongestConsec(new string[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1);
+            foreach (var b in a)
+            {
+                Console.WriteLine(b);   
+            }
+        }
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            if(!(strarr.Length == 0 || k >strarr.Length || k<= 0))
+            {
+                string[] Guardador = new string[strarr.Length];
+                for (int i = 0; i < strarr.Length - k ; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                        Guardador[i] += strarr[j + i];
+                }
+
+                return k != 1?  Guardador.Where(s2 => s2 != null).OrderByDescending(s1 => s1.Length).FirstOrDefault() : strarr.Where(s2 => s2 != null).OrderByDescending(s1 => s1.Length).FirstOrDefault();
+            }
+            return "";
+        }
+    }
+
+}
